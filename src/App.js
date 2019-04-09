57440bacb0b912740be8e160742f767620548991
Shubham Vishwakarma <tcet.shubham@gmail.com>
2019-04-09 20:27:52 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import reactCSS from "reactcss";
+import CustomPicker from "./component/ColorPicker";
+
+const hex2rgba = hex => {
+  const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+  return {
+    r,
+    g,
+    b,
+    a: 1
+  };
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayColorPicker: false,
+      color: {
+        r: "241",
+        g: "112",
+        b: "19",
+        a: "1"
+      }
+    };
+    this.colorList = [
+      "#CA2121",
+      "#7E1515",
+      "#D5F200",
+      "#F9F9F9",
+      "#B0F8FD",
+      "#FFFFFF",
+      "#FECF9C",
+      "#E3A2FC",
+      "#FDF39B",
+      "#A3FFAE",
+      "#C9C9C9",
+      "#FFBABD",
+      "#B4FDD1",
+      "#B6B6B6",
+      "#C1D4FD",
+      "#FE83A8",
+      "#E64C3D",
+      "#E67D22",
+      "#EFC20D",
+      "#8BC249",
+      "#2DCC70",
+      "#19BB9A",
+      "#D62941",
+      "#673BB7"
+      // "#E81B61"
+    ];
+  }
+
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false });
+  };
+
+  handleChange = color => {
+    console.log(color);
+    this.setState({ color: color.rgb });
+  };
+
+  onApplyColor = value => {
+    value
+      ? this.handleChange({
+          rgb: hex2rgba(value)
+        })
+      : this.handleClose();
+    console.log("Apply");
+  };
+
   render() {
+    const styles = reactCSS({
+      default: {
+        backgroundStyle: {
+          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${
+            this.state.color.b
+          }, ${this.state.color.a})`
+        },
+        color: {
+          width: "36px",
+          height: "14px",
+          borderRadius: "2px",
+          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${
+            this.state.color.b
+          }, ${this.state.color.a})`
+        },
+        swatch: {
+          padding: "5px",
+          background: "#fff",
+          borderRadius: "1px",
+          boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+          display: "inline-block",
+          cursor: "pointer"
+        },
+        popover: {
+          position: "absolute",
+          zIndex: "2"
+        },
+        cover: {
+          position: "fixed",
+          top: "0px",
+          right: "0px",
+          bottom: "0px",
+          left: "0px"
+        }
+      }
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={styles.backgroundStyle}>
+        <div style={styles.swatch} onClick={this.handleClick}>
+          <div style={styles.color} />
+        </div>
+        {this.state.displayColorPicker ? (
+          <div style={styles.popover}>
+            {/* <div style={styles.cover} onClick={this.handleClose} /> */}
+            <CustomPicker
+              color={this.state.color}
+              onChange={this.handleChange}
+              onApplyColor={this.onApplyColor}
+              colorList={this.colorList}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
---
+++ src/component/ColorPicker.jsx
@@ -0,0 +1,122 @@
+import { CustomPicker } from "react-color";
+import {
+  Saturation,
+  Hue,
+  EditableInput,
+  Checkboard,
+  Alpha
+} from "react-color/lib/components/common";
+// import {} from "react-color/lib/components/circle";
+import React from "react";
+
+const ColorPicker = props => {
+  let inputStyle = {
+    input: {
+      border: "none",
+      borderBottom: "1px solid rgba(216, 216, 216, 0.38)",
+      padding: "8px 5px 2px 0",
+      margin: "0",
+      width: "80%",
+      background: "0 0",
+      color: "#c0c0c1"
+    },
+    label: {
+      fontSize: "12px",
+      color: "#999"
+    }
+  };
+  const paintColorSwatch = () => {
+    let colorSwatchMarkup = [];
+    props.colorList.map(value => {
+      colorSwatchMarkup.push(
+        <div
+          style={{
+            backgroundColor: value
+          }}
+          className="color-swatch-div"
+          onClick={() => props.onApplyColor(value)}
+        />
+      );
+    });
+    return <React.Fragment>{colorSwatchMarkup}</React.Fragment>;
+  };
+  return (
+    <div className="color-picker">
+      <div className="header-div">Select Color</div>
+      <div className="wrapper-divs">
+        <div className="left-wrapper">
+          <div className="color-list-div">
+            <div className="my-color-header">My Colors</div>
+            {paintColorSwatch()}
+          </div>
+          <div className="pos-rel" style={{ marginTop: "20px" }}>
+            <div className="sub-header">Transparency</div>
+            <div className="alpha-wrapper">
+              <Alpha
+                {...props}
+                pointer={CustomPointer}
+                onChange={props.onChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="right-wrapper">
+          <div className="sat-hue-container">
+            <div className="st-wrapper">
+              <Saturation
+                {...props}
+                pointer={CustomPointer}
+                onChange={props.onChange}
+              />
+            </div>
+            <div className="hue-wrapper">
+              <Hue
+                {...props}
+                direction="vertical"
+                pointer={CustomPointer}
+                onChange={props.onChange}
+              />
+            </div>
+          </div>
+          <div className="input-check-cont">
+            <div className="input-wrapper">
+              <EditableInput
+                label={"Selected Color"}
+                value={props.hex}
+                onChange={props.onChange}
+                style={inputStyle}
+              />
+            </div>
+            <div className="check-wrapper">
+              <div
+                style={{
+                  zIndex: 2,
+                  backgroundColor: `rgba(${props.color.r},${props.color.g},${
+                    props.color.b
+                  },${props.color.a})`,
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  right: "0",
+                  bottom: "0"
+                }}
+              />
+              <Checkboard white="#fff" grey="#333" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-div">
+        <button className="apply-custom-btn" onClick={props.onApplyColor}>
+          Apply
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const CustomPointer = props => {
+  return <div className="color-picker-pointer" />;
+};
+
+export default CustomPicker(ColorPicker);
